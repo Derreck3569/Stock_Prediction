508fcea26774d561b6633d7a89b2c48b10b35d49
Derreck3569 <caohoangquan21032004@gmail.com>
2026-02-19 17:13:05 -0600

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -20,7 +20,7 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['NVDA','APPL', 'TMC','FDX']
+    stk_tickers = ['NVDA','AAPL', 'TMC','FDX']
     ccy_tickers = ['DEXJPUS', 'DEXUSUK']
     idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
@@ -32,7 +32,7 @@ def extract_features():
     Y = np.log(stk_data.loc[:, ('Adj Close', 'NVDA')]).diff(return_period).shift(-return_period)
     Y.name = Y.name[-1]+'_Future'
     
-    X1 = np.log(stk_data.loc[:, ('Adj Close', ('APPL', 'TMC','FDX'))]).diff(return_period)
+    X1 = np.log(stk_data.loc[:, ('Adj Close', ('AAPL', 'TMC','FDX'))]).diff(return_period)
     X1.columns = X1.columns.droplevel()
     X2 = np.log(ccy_data).diff(return_period)
     X3 = np.log(idx_data).diff(return_period)
@@ -66,6 +66,7 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
 
